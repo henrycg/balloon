f786c2e66be40527da88da08d7347ed33a59e4c0
Henry Corrigan-Gibbs <henrycg-git@cs.stanford.edu>
2015-09-23 15:49:52 -0700

    Util test code
    * Splitting out file with MIN/MAX constants
    * Test for arg checking

---
+++ libbaghash/constants.h
@@ -0,0 +1,14 @@
+#ifndef __CONSTANTS_H__
+#define __CONSTANTS_H__
+
+#define OUTLEN_MIN 16
+#define OUTLEN_MAX 128
+
+#define INLEN_MIN 4
+#define INLEN_MAX (1<<20)
+
+#define SALTLEN_MIN 4
+#define SALTLEN_MAX (1<<20)
+
+#endif
+
--- libbaghash/util.c
+++ libbaghash/util.c
@@ -1,33 +1,25 @@
 
 #include <baghash.h>
 
+#include "constants.h"
 #include "errors.h"
-
-#define OUTLEN_MIN 16
-#define OUTLEN_MAX 128
-
-#define INLEN_MIN 4
-#define INLEN_MAX (1<<20)
-
-#define SALTLEN_MIN 4
-#define SALTLEN_MAX (1<<20)
 
 int
 validate_parameters (size_t outlen, size_t inlen, size_t saltlen)
 {
   if (outlen < OUTLEN_MIN)
     return ERROR_OUTLEN_TOO_SMALL;
-  if (outlen > OUTLEN_MAX)
+  if (outlen >= OUTLEN_MAX)
     return ERROR_OUTLEN_TOO_BIG;
 
   if (inlen < INLEN_MIN)
     return ERROR_INLEN_TOO_SMALL;
-  if (inlen > INLEN_MAX)
+  if (inlen >= INLEN_MAX)
     return ERROR_INLEN_TOO_BIG;
 
   if (saltlen < SALTLEN_MIN)
     return ERROR_SALTLEN_TOO_SMALL;
-  if (saltlen > SALTLEN_MAX)
+  if (saltlen >= SALTLEN_MAX)
     return ERROR_SALTLEN_TOO_BIG;
 
   return 0;
